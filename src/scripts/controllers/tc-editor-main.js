9b57e2c6f069b4e053dc65cc3236f69ff952218a
Dario Carabajal <dariocarabajal@MLBA-OSX-DCARABAJAL.local>
2015-03-16 12:17:21 -0300

    tags collection from advanced->basic

--- src/scripts/controllers/tc-editor-main.js
+++ src/scripts/controllers/tc-editor-main.js
@@ -50,45 +50,26 @@
           }
         };
 
-        $scope.availableTags = ['sanity', 'regression', 'ui'];
+        $scope.availableTags = tags($scope.list );
 
         $scope.$on('event:advanced-editor-parsed', safeApplyWrapper($scope, function wrapAdvancedEdition(event, doc) {
-          updateAvailableTags(doc);
-          //$scope.$broadcast('event:advanced-editor-edited', doc);
+          $scope.availableTags = tags(doc);
+          $scope.$broadcast('event:advanced-editor-edited', doc);
         }));
 
-        function updateAvailableTags(doc){
+        function tags(obj) {
+          var values = _(obj).values();
 
-          console.log(getUniqueTags(doc));
-          $scope.availableTags = ['sanity', 'regression', 'ui'];
-        };
+          if (values.isEmpty()){
+            return [];
+          }
 
-        function pickDeep(collection, identity, thisArg) {
-          var picked = _.pick(collection, identity, thisArg);
-          var collections = _.pick(collection, _.isObject, thisArg);
-
-          _.each(collections, function(item, key, collection) {
-            var object;
-            if (_.isArray(item)) {
-              object = _.reduce(item, function(result, value) {
-                var picked = pickDeep(value, identity, thisArg);
-                if (!_.isEmpty(picked)) {
-                  result.push(picked);
-                }
-                return result;
-              }, []);
-            } else {
-              object = pickDeep(item, identity, thisArg);
-            }
-
-            if (!_.isEmpty(object)) {
-              picked[key] = object;
-            }
-
-          });
-
-          return picked;
+          return _.union(
+            values.pluck("tags").flatten().value(),
+            values.pluck("steps").flatten().map(tags).flatten().value()
+          );
         }
+
 
 
       })();
